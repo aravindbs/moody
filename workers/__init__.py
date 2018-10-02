7660a80593ceada3ec94d4f8738444c1d97816e9
Aravind B.S <bs.aravind.97@gmail.com>
2018-10-02 15:38:00 +0530

    heroku init

--- workers/__init__.py
+++ workers/__init__.py
@@ -1,8 +1,15 @@
 import pymongo
-import yaml
-
-with open("../config.yml", "r") as f:
-    config = yaml.load(f)
+import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
+load_dotenv(dotenv_path='.env')
+with open("config.json", "r") as f:
+    config = json.load(f)
+    #load_dotenv()
+    for key, value in config.items():
+        for _key, _value in value.items():
+            value[_key] = os.environ[_key]
 
 myclient = pymongo.MongoClient(config['mongodb']['MONGO_URI'])
 db = myclient.testmoody
