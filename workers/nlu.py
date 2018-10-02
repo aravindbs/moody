e028680ef57d9dd39e753d7ff7613639b7e6f739
Aravind B.S <bs.aravind.97@gmail.com>
2018-10-02 23:38:59 +0530

    dashboard fix

--- workers/nlu.py
+++ workers/nlu.py
@@ -31,49 +31,6 @@ while(1):
         print(user['username'])
         anger = fear = disgust = joy = sadness = count = 0
         tweets = db.tweets.find({'username' : user['username']})
-<<<<<<< HEAD
-        tweets = dict(list(tweets)[0])
-        #print(tweets)
-        tweets.pop('_id', None)
-        tweets = tweets['tweets']
-        #print(type(tweets))
-        
-        for key in tweets: 
-            if int(key) < 30: 
-                values = tweets[key]
-                #print(values)
-                anger = disgust = fear = joy = sadness = count = 0
-                for tweet in values: 
-                    text = tweet['text']
-                    #time = tweet['created_at']
-                    tone = tone_analyzer.tone({'text': text},'application/json').get_result()
-                    if tone is None:
-                        continue 
-                    anger = anger + tone['document_tone']['tone_categories'][0]['tones'][0]['score']
-                    disgust = disgust + tone['document_tone']['tone_categories'][0]['tones'][1]['score']
-                    fear = fear + tone['document_tone']['tone_categories'][0]['tones'][2]['score']
-                    joy = joy + tone['document_tone']['tone_categories'][0]['tones'][3]['score']
-                    sadness = sadness + tone['document_tone']['tone_categories'][0]['tones'][4]['score']
-                    count = count + 1
-                try:
-                    response = natural_language_understanding.analyze(text=text,features=Features(entities=EntitiesOptions(emotion=True,sentiment=True,limit=2),keywords=KeywordsOptions(emotion=True,sentiment=True,limit=5))).get_result()
-                except: 
-                    continue
-                keywords = response['keywords']
-                for keyword in keywords: 
-                    db_keywords.append(keyword['text'])
-                emotions = {}
-                emotions['anger'] = anger = anger / count 
-                emotions['disgust'] = disgust = disgust / count 
-                emotions['fear'] = fear = fear / count 
-                emotions['joy'] = joy = joy / count 
-                emotions['sadness'] = sadness = sadness / count
-                emotions['day'] = key
-                #print(emotions['day'])
-                all_emotions.append(emotions)
-                    
-                #print(all_emotions)
-=======
         print(list(tweets))
         if list(tweets) is not []: 
             try:
@@ -120,7 +77,6 @@ while(1):
                         #print(all_emotions)
             except: 
                 pass             
->>>>>>> a1c6499e89c5d43db9831932d09cd7a4b33eb287
         query = { 'screen_name' : user['twitter_handle']}
         update = { 'username' : user['username'] , 'screen_name' : user['twitter_handle'] , 'emotions' : all_emotions, 'keywords' : db_keywords }
         db.emotions.update(query, update, upsert=True)
